4097aa4443c21722f647a8b32639db9cf2f8edd0
josephvarghese78 <37336072+josephvarghese78@users.noreply.github.com>
2026-02-22 22:32:44 -0500

    Restore performance metrics SQL queries
    Reintroduced SQL queries for performance metrics including min, max, avg, median, and percentiles for test names and suites, along with adjustments for response time calculations.

--- bench.sql
+++ bench.sql
@@ -62,4 +62,46 @@ SELECT
     (SELECT response_time FROM ordered WHERE rn = CAST(0.9 * cnt AS INT)) AS percentile_90,
     (SELECT response_time FROM ordered WHERE rn = CAST(0.95 * cnt AS INT)) AS percentile_95,
     (SELECT response_time FROM ordered WHERE rn = CAST(0.99 * cnt AS INT)) AS percentile_99
+
 FROM performance;
+
+
+
+all all all
+WITH ordered AS (
+    SELECT suite_name, test_name, response_time/1000 as response_time, max_threads as test,
+           ROW_NUMBER() OVER (PARTITION BY suite_name, test_name ORDER BY response_time/1000) AS rn,
+           COUNT(*) OVER (PARTITION BY suite_name, test_name) AS cnt
+    FROM performance
+)
+SELECT suite_name, test, test_name,
+       MIN(response_time) AS min_response_time,
+       MAX(response_time) AS max_response_time,
+       AVG(response_time) AS avg_response_time,
+       (SELECT response_time FROM ordered o2 
+        WHERE o2.suite_name = o1.suite_name AND o2.test_name = o1.test_name AND rn = (cnt + 1)/2) AS median_response_time,
+        (SELECT response_time FROM ordered o2 
+        WHERE o2.suite_name = o1.suite_name AND o2.test_name = o1.test_name AND rn = CAST(0.1 * cnt AS INT)) AS percentile_10,
+        (SELECT response_time FROM ordered o2 
+        WHERE o2.suite_name = o1.suite_name AND o2.test_name = o1.test_name AND rn = CAST(0.2 * cnt AS INT)) AS percentile_20,
+        (SELECT response_time FROM ordered o2 
+        WHERE o2.suite_name = o1.suite_name AND o2.test_name = o1.test_name AND rn = CAST(0.3 * cnt AS INT)) AS percentile_30,
+        (SELECT response_time FROM ordered o2 
+        WHERE o2.suite_name = o1.suite_name AND o2.test_name = o1.test_name AND rn = CAST(0.4 * cnt AS INT)) AS percentile_40,
+        (SELECT response_time FROM ordered o2 
+        WHERE o2.suite_name = o1.suite_name AND o2.test_name = o1.test_name AND rn = CAST(0.5 * cnt AS INT)) AS percentile_50,
+        (SELECT response_time FROM ordered o2 
+        WHERE o2.suite_name = o1.suite_name AND o2.test_name = o1.test_name AND rn = CAST(0.6 * cnt AS INT)) AS percentile_60,
+        (SELECT response_time FROM ordered o2 
+        WHERE o2.suite_name = o1.suite_name AND o2.test_name = o1.test_name AND rn = CAST(0.7 * cnt AS INT)) AS percentile_70,
+        (SELECT response_time FROM ordered o2 
+        WHERE o2.suite_name = o1.suite_name AND o2.test_name = o1.test_name AND rn = CAST(0.8 * cnt AS INT)) AS percentile_80,
+       (SELECT response_time FROM ordered o2 
+        WHERE o2.suite_name = o1.suite_name AND o2.test_name = o1.test_name AND rn = CAST(0.9 * cnt AS INT)) AS percentile_90,
+       (SELECT response_time FROM ordered o2 
+        WHERE o2.suite_name = o1.suite_name AND o2.test_name = o1.test_name AND rn = CAST(0.95 * cnt AS INT)) AS percentile_95,
+       (SELECT response_time FROM ordered o2 
+        WHERE o2.suite_name = o1.suite_name AND o2.test_name = o1.test_name AND rn = CAST(0.99 * cnt AS INT)) AS percentile_99
+FROM ordered o1
+GROUP BY suite_name, test, test_name
+order by test 
